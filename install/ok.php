4a82af3b12e8d0bcc202b96dfc3d0036a1def95e
j0rpi <webmaster@j0rpi.net>
2015-01-11 21:06:46 +0100

    Create ok.php

---
+++ install/ok.php
@@ -0,0 +1,30 @@
+<!--
+
+ img_concept (working name)
+ file: ok.php
+ purpose: displayed when script is installed..
+
+--!>
+
+<style type='text/css'>
+
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;}
+.tg td{margin-left: 50px; text-align: left;font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
+.tg th{margin-left: 50px; text-align: left;font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
+.container {background: url('http://bibben.se/wp-content/uploads/2013/07/background.png'); width: 800px; height: 300px; border-radius: 8px 8px 8px 8px; border: 1px solid black;}
+</style>
+<center>
+<div align='center' class='container'>
+<font style='font-size: 18px; font-family: Arial;'><b>Img Concept Installer</b></font><br>
+<font style='font-size: 12px; font-family: Arial;'>This installer is not 100% accurate and my fuck up your installation.
+<br>
+</font>
+<br>
+<br>
+<br>
+<br>
+<br>
+<font style='font-size: 16px; font-family: Arial;'><b>imgconcept has been successfully installed. click <a href='../index.php'>here to go to site.</a></b></font>
+</center>
+</div>"
+
